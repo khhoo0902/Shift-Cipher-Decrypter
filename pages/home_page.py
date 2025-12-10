85799b3f69fd8cfe84b4b5c766b4d0473cadc873
Hooo <95530187+khhoo0902@users.noreply.github.com>
2025-12-10 21:28:38 +0800

    Add files via upload

--- pages/home_page.py
+++ pages/home_page.py
@@ -7,17 +7,15 @@ st.subheader("This web app is designed to decrypt messages that are encrypted wi
 
 st.space("large")
 st.header("Would you like to...", anchor=False)
-left, right = st.columns(2, gap="small", border=False)
+left, right = st.columns(2, gap="small")
 with left:
-    st.page_link(page="pages/encryptor_page.py", label="**:blue-background[Try encryption at the encrypter page]**", width="content")
+    st.page_link(page="pages/encryptor_page.py", label="**:blue-background[Try encrypting at the encrypter page]**", width="content")
 with right:
-    st.page_link(page="pages/decryptor_page.py", label="**:blue-background[Try decryption at the decrypter page]**", width="content")
+    st.page_link(page="pages/decryptor_page.py", label="**:blue-background[Try decrypting at the decrypter page]**", width="content")
 
 st.divider()
 st.markdown("""
             * Please note that the *:red-background[results are reserved]* because the algorithm must not be 100% accurate.
 
             * If you don't know what Shift Cipher encryption is, you can read about it in this [Wikipedia page](https://en.wikipedia.org/wiki/Caesar_cipher).
-
             """)
-
