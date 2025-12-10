2c6af549271a5d63c98e0f2f35f241bf873a5695
Hooo <95530187+khhoo0902@users.noreply.github.com>
2025-12-10 21:20:09 +0800

    Update home_page.py

--- pages/home_page.py
+++ pages/home_page.py
@@ -6,7 +6,7 @@ st.title("Welcome to this :rainbow[Shift Cipher decryptor]! :unlock:", anchor=Fa
 st.subheader("This web app is designed to decrypt messages that are encrypted with the Shift Cipher encryption method.", anchor=False)
 
 st.space("large")
-st.header("Would you want to...", anchor=False)
+st.header("Would you like to...", anchor=False)
 left, right = st.columns(2, gap=None, border=False)
 with left:
     st.page_link(page="pages/encryptor_page.py", label="**:blue-background[Try encryption at the encrypter page]**", width="content")
@@ -18,4 +18,5 @@ st.markdown("""
             * Please note that the *:red-background[results are reserved]* because the algorithm must not be 100% accurate.
 
             * If you don't know what Shift Cipher encryption is, you can read about it in this [Wikipedia page](https://en.wikipedia.org/wiki/Caesar_cipher).
+
             """)
