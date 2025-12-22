428a244731ea10162e8afc6eff2f32516eb53fac
Hooo <95530187+khhoo0902@users.noreply.github.com>
2025-12-23 01:08:13 +0800

    Add files via upload

--- pages/home_page.py
+++ pages/home_page.py
@@ -1,21 +1,27 @@
 import streamlit as st
 
-st.sidebar.success("Start with the tabs above!")
-
-st.title("Welcome to this :rainbow[Shift Cipher decryptor]! :unlock:", anchor=False)
+#Intro
+st.title("Welcome to this :rainbow[Shift Cipher Decrypter]! :unlock:", anchor=False)
 st.subheader("This web app is designed to decrypt messages that are encrypted with the Shift Cipher encryption method.", anchor=False)
 
+#Navigate user
 st.space("large")
 st.header("Would you like to...", anchor=False)
-left, right = st.columns(2, gap="small")
-with left:
-    st.page_link(page="pages/encryptor_page.py", label="**:blue-background[Try encrypting at the encrypter page]**", width="content")
-with right:
-    st.page_link(page="pages/decryptor_page.py", label="**:blue-background[Try decrypting at the decrypter page]**", width="content")
+col1, col2 = st.columns(2, gap="small")
+with col1:
+    st.page_link(page="pages/encrypter_page.py", label="**:blue-background[Try encrypting at the encrypter page]**", width="content")
+with col2:
+    st.page_link(page="pages/decrypter_page.py", label="**:blue-background[Try decrypting at the decrypter page]**", width="content")
 
+#Remarks
 st.divider()
 st.markdown("""
-            * Please note that the *:red-background[decryption results are reserved]* because the algorithm must not be 100% accurate.
+            * This decrypter is designed to decrypt ***:red-background[English message only]***.
+
+            * Please note that the ***:red-background[decryption results are reserved]*** because the algorithm must not be 100% accurate.
 
             * If you don't know what Shift Cipher encryption is, you can find more about it in this [Wikipedia Page](https://en.wikipedia.org/wiki/Caesar_cipher).
             """)
+
+#Sidebar
+st.sidebar.success("Get started with the tabs above!", icon=":material/arrow_upward:")
